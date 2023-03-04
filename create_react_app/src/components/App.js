ef1cb27869ac44589d1db6a7fb5086a8b8245013
MaevaPrecourt <maeva.precourt@gmail.com>
2023-03-04 15:01:03 +0100

    Create React App - 4/3

---
+++ create_react_app/src/components/App.js
@@ -0,0 +1,5 @@
+import Banner from "./Banner"
+function App(){
+  return <Banner/>
+}
+export default App
---
+++ create_react_app/src/components/Banner.js
@@ -0,0 +1,4 @@
+function Banner(){
+    return <h1>La Maison Jungle</h1>
+}
+export default Banner
